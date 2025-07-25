f0d70b84da1220056753aa1365d50e73d0a7dbf4
Momchil Kalestrov <kalestrov.mo@gmail.com>
2025-07-25 09:04:09 +0300

    add a color picker

---
+++ src/components/propertiesPanel/colorPicker.tsx
@@ -0,0 +1,46 @@
+'use client';
+import React from 'react';
+import { PaintbrushVertical } from 'lucide-react';
+import { HexAlphaColorPicker } from 'react-colorful';
+import { Button } from '@/components/ui/button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Input } from '@/components/ui/input';
+
+type Props = {
+    value: string;
+    selected?: boolean;
+    onChange: (newValue: string) => void;
+};
+
+const ColorPicker: React.FC<Props> = ({ value: initialColor, selected = true, onChange: onChangeCallback }) => {
+    const [ color, setColor ] = React.useState<string>('');
+
+    React.useEffect(() => {
+        setColor(initialColor.startsWith('#') ? initialColor : '#ffffff');
+    }, []);
+
+    const onChange = React.useCallback((newColor: string) => {
+        setColor(newColor);
+        onChangeCallback(newColor);
+    }, []);
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button variant={ selected ? 'outline' : 'ghost' } size='icon' className='size-8 p-2'>
+                    <PaintbrushVertical />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className='flex flex-col gap-2'>
+                <HexAlphaColorPicker
+                    color={ color }
+                    onChange={ onChange }
+                    style={ { width: '100%' } }
+                />
+                <Input value={ color } onChange={ (e) => onChange(e.target.value) } />
+            </PopoverContent>
+        </Popover>
+    );
+};
+
+export default ColorPicker;
