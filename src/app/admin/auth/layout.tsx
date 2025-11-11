7334fae7d5dcb8b3dc917b0c2c4b335b27ab71b8
Momchil Kalestrov <kalestrov.mo@gmail.com>
2025-11-11 20:51:50 +0200

    fix: metadata exported outside the client component

--- src/app/admin/auth/layout.tsx
+++ src/app/admin/auth/layout.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Metadata } from 'next';
 //@ts-ignore
 import '../globals.css';
+
+export const metadata: Metadata = {
+    title: 'Login'
+};
+
 export default ({ children }: { children: React.ReactNode }) => <React.Suspense>{ children }</React.Suspense>;
