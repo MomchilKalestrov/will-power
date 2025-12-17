66d2c0388f7badb482d09f7116035f5492d3ad64
Momchil Kalestrov <kalestrov.mo@gmail.com>
2025-12-17 23:12:24 +0200

    refactor: add better formatting for code blocks in the plugin viewer page

--- src/app/admin/plugins/marketplace/plugin/[name]/page.tsx
+++ src/app/admin/plugins/marketplace/plugin/[name]/page.tsx
@@ -6,12 +6,14 @@ import { NextPage } from 'next';
 import Markdown from 'react-markdown';
 import MonacoEditor from '@monaco-editor/react';
 
-import { getPlugin } from '@/lib/actions';
 import { Button } from '@/components/ui/button';
 import { Spinner } from '@/components/ui/spinner';
 import { Separator } from '@/components/ui/separator';
-import { usePlugins } from '@/contexts/plugins';
+
 import { useDialog } from '@/contexts/dialog';
+import { usePlugins } from '@/contexts/plugins';
+
+import { getPlugin } from '@/lib/actions';
 
 type plugin = (Awaited<ReturnType<typeof getPlugin>> & { success: true; })[ 'value' ];
 
@@ -19,6 +21,15 @@ const getLanguage = (lang: string): string => ({
     ts: 'typescript',
     js: 'javascript'
 })[ lang ] || lang;
+
+const countNewlines = (str: string): number => {
+    let counter = 0;
+
+    for (const letter of str) 
+        if (letter === '\n') counter++;
+
+    return counter;
+};
 
 const Page: NextPage<PageProps<'/admin/plugins/marketplace/plugin/[name]'>> = ({ params }) => {
     const { name } = React.use(params);
@@ -68,7 +79,7 @@ const Page: NextPage<PageProps<'/admin/plugins/marketplace/plugin/[name]'>> = ({
         );
 
     return (
-        <div className='h-[calc(100dvh-var(--spacing)*16)] p-8 box-border'>
+        <div className='p-8 box-border'>
             <header className='grid grid-cols-[auto_1fr_auto] gap-2'>
                 <img src={ plugin.iconUrl } alt={ `${ plugin.name } icon` } className='size-20 row-span-full' />
                 <div className='flex flex-col justify-center'>
@@ -90,21 +101,43 @@ const Page: NextPage<PageProps<'/admin/plugins/marketplace/plugin/[name]'>> = ({
                     children={ plugin.description }
                     components={ {
                         code: ({ children = '', className }) => {
-                            const match = /language-(\w+)/.exec(className || '');
+                            const [ text, match ] = typeof children === 'object'
+                            ?   [ JSON.stringify(children, null, '    '), 'json' ]
+                            :   [ children.toString(), /language-(\w+)/.exec(className || '') ];
+
                             const language = match !== null && (match?.length ?? 0) >= 2 ? match[ 1 ] : '';
+                            const lines = countNewlines(text);
 
                             return (
-                                <MonacoEditor
-                                    className='h-64 max-w-2xl w-full rounded-md overflow-hidden border'
-                                    options={ { readOnly: true } }
-                                    value={ String(children) }
-                                    language={ getLanguage(language) }
-                                    theme={
-                                        document.body.classList.contains('dark')
-                                        ?   'vs-dark'
-                                        :   'light'
-                                    }
-                                />
+                                <div
+                                    style={ { '--lines': Math.min(lines, 10) } as React.CSSProperties }
+                                    className='w-full max-w-2xl h-[calc(var(--lines)*19px+2px)] p-2 border box-content'
+                                >
+                                    <MonacoEditor
+                                        options={ {
+                                            readOnly: true,
+                                            scrollbar: {
+                                                vertical: lines > 10 ? 'visible' : 'hidden',
+                                                horizontal: 'hidden',
+                                                handleMouseWheel: lines > 10
+                                            },
+                                            minimap: {
+                                                enabled: false
+                                            },
+                                            overviewRulerLanes: 0,
+                                            overviewRulerBorder: false,
+                                            scrollBeyondLastLine: false,
+                                            lineNumbers: 'off'
+                                        } }
+                                        value={ String(children) }
+                                        language={ getLanguage(language) }
+                                        theme={
+                                            document.body.classList.contains('dark')
+                                            ?   'vs-dark'
+                                            :   'light'
+                                        }
+                                    />
+                                </div>
                             );
                         }
                     } }
